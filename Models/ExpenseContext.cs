1cddef1c1e624b9f2a3fc7b6f9cb86dce960464e
Andrei Preotu <andrei.preotu.g@gmail.com>
2020-11-07 17:00:12 +0200

    Add project files.

---
+++ Models/ExpenseContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Preotu_Andrei_Lab5.Models
+{
+    public class ExpenseContext : DbContext
+    {
+        public ExpenseContext(DbContextOptions<ExpenseContext> options)
+        : base(options)
+        {
+        }
+        public DbSet<Expense> ExpenseDTO { get; set; }
+        public DbSet<Expense> Expenses { get; set; }
+        public object Expense { get; internal set; }
+    }
+}

9d97580ad6b943bf099267e847153568c55ed8ce
Andrei Preotu <andrei.preotu.g@gmail.com>
2020-11-14 23:19:11 +0200

    lab 5

--- Models/ExpenseContext.cs
+++ Models/ExpenseContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Preotu_Andrei_Lab5.Models;
 
 namespace Preotu_Andrei_Lab5.Models
 {
@@ -12,8 +13,9 @@ namespace Preotu_Andrei_Lab5.Models
         : base(options)
         {
         }
-        public DbSet<Expense> ExpenseDTO { get; set; }
+        public DbSet<ExpenseDTO> ExpenseDTO { get; set; }
         public DbSet<Expense> Expenses { get; set; }
         public object Expense { get; internal set; }
+        public DbSet<Budget> Budget { get; set; }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using Preotu_Andrei_Lab5;
 using Preotu_Andrei_Lab5.Models;
 
@@ -30,17 +31,42 @@ namespace Preotu_Andrei_Lab5
         {
             services.AddDbContext<ExpenseContext>(opt => opt.UseInMemoryDatabase("ExpenseList"));
             services.AddControllers();
+            services.AddSwaggerGen();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Expenses API",
+                    Description = "A simple example ASP.NET Core Web API",
+                    TermsOfService = new Uri("https://econ.ubbcluj.ro/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Nume Prenume",
+                        Email = string.Empty,
+                        Url = new Uri("https://econ.ubbcluj.ro/cv.php?id=540"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under LICX",
+                        Url = new Uri("https://econ.ubbcluj.ro/licence"),
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseSwagger();
 
-            app.UseHttpsRedirection();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My Expenses API v1.1");
+            });
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
 
             app.UseRouting();
 
